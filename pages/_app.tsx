110f81b8d276a6a95c6d36556b9f708ea48b2305
Reza Firdaus <azerus98@gmail.com>
2022-12-01 09:39:23 +0700

    connect to websocket(actioncable)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import dynamic from 'next/dynamic'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const Navigasi = dynamic(() => import("../components/navigasi"), {ssr: false})
+
+  return (
+    <>
+      <Navigasi />
+      <Component {...pageProps} />
+    </>
+  )
 }
