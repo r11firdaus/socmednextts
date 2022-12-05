211607e367566e047fb16291fbb5935a65533116
Reza Firdaus <azerus98@gmail.com>
2022-12-05 09:44:03 +0700

    show comment

--- pages/post/[post_id].jsx
+++ pages/post/[post_id].jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 
 export const getServerSideProps = async (ctx) => {
   const { query } = ctx
@@ -17,6 +17,26 @@ export const getServerSideProps = async (ctx) => {
 
 const postDetail = (props) => {
   const { data } = props
+  const [comments, setcomments] = useState([])
+
+  useEffect(() => {
+    async function fetchData () {
+      const res = await fetch(`http://localhost:4000/api/v1/comments?post_id=${data.id}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      if (res.status == 200) {
+        const resJson = await res.json()
+        setcomments(resJson.data)
+        console.log(resJson.data)
+      }
+    }
+    fetchData()
+  }, [])
+  
+
   return (
     <div className='container mt-5 pt-5 text-light'>
       <div className="card my-2 border border-light bg-dark px-3 py-3">
@@ -25,6 +45,16 @@ const postDetail = (props) => {
       </div>
 
       <div className="pl-3 py-3"><h5>Comments</h5></div>
+      <div className="container">
+        { comments.map(e => (
+          <div className="card border-secondary bg-dark" key={e.id}>
+            <div className="card-body">
+              <strong>{e.email}</strong>
+              <p>{e.content}</p>
+            </div>
+          </div>
+        )) }
+      </div>
     </div>
   )
 }
