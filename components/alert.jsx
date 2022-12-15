c78313d15ff7a787813de23cf1aed300a547edb3
Reza Firdaus <azerus98@gmail.com>
2022-12-15 15:29:11 +0700

    refactor

--- components/alert.jsx
+++ components/alert.jsx
@@ -1,7 +1,8 @@
 const Alert = (props) => {
-  return (
-    <div className={`alert alert-${props.type} alert-dismissible fade show`} role="alert">
-      <strong>Horray!</strong> You successfully add new post.
+  const { status, statusText, type } = props.data
+    return (
+    <div className={`alert alert-${type} alert-dismissible fade show`} role="alert">
+      <strong>{status}</strong> {statusText}
       <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
   )
