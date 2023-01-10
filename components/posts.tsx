5a6f1c836b631c8a285eeabe27c996aeb6ac8956
Reza Firdaus <azerus98@gmail.com>
2023-01-10 13:14:51 +0700

    add spinner, update profile page

--- components/posts.tsx
+++ components/posts.tsx
@@ -10,7 +10,7 @@ const Posts = (props: { posts: PostsTypes[] }): JSX.Element => {
     { posts.map((e) => (
       <div className="card my-2 border border-secondary bg-dark text-light" key={e.id}>
         <div className="card-body">
-          <strong>{e.email}</strong>
+          <Link href={`/profile/${e.email}`}><strong>{e.email}</strong></Link>
           <span className="px-2 py-2"><small className="text-secondary mr-3">{setDateTime(e.created_at)}</small></span>
           <p>{e.content}</p>
           <div className="position-absolute bottom-0 end-0 px-2 py-2"><Link href={`/post/${e.id}`}>Comments</Link></div>
--- pages/profile/[user_email].tsx
+++ pages/profile/[user_email].tsx
@@ -3,9 +3,11 @@ import dynamic from "next/dynamic"
 import { useEffect, useState } from "react"
 import { getAPI } from "../../lib/callAPI"
 import { getData } from "../../lib/dataStore"
+import PostsTypes from "../../types/posts"
 import UserTypes from "../../types/users"
 
 const Spinner = dynamic(() => import('../../components/spinner'), { ssr: false })
+const Posts = dynamic(() => import('../../components/posts'), { ssr: false })
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const { query } = ctx
@@ -17,6 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 const userpage = (props: { user_email: string }): JSX.Element => {
   const [loading, setloading] = useState(true)
   const [userData, setuserData] = useState<UserTypes>({ user_id: 0 })
+  const [posts, setposts] = useState<PostsTypes[]>([])
 
   useEffect(() => {
     fetchData()
@@ -26,39 +29,57 @@ const userpage = (props: { user_email: string }): JSX.Element => {
   }, [])
 
   const fetchData = async () => {
+    let datauser = userData
     const email = getData('email', 0)
+
     if (props.user_email == email) {
       const user = {
         email,
         user_id: getData('user_id', 0)
       }
 
-      setuserData(user)
+      datauser = user
     } else {
       const res = await getAPI({path: `users/0?email=${props.user_email}`})
-      const userdata: UserTypes = res.data
       
-      if (userdata) {
-        setuserData(userdata)
-      } else console.log(res.message)
+      if (res.data) {
+        const changeResStructure: UserTypes = {
+          ...res.data,
+          user_id: res.data.id
+        }
+        datauser = changeResStructure 
+      }
+      else console.log(res.message)
     }
 
+    if (datauser.email) {
+      setuserData(datauser)
+      const user_posts = await getAPI({ path: `user_posts?user_id=${datauser.user_id}` })
+      user_posts.data && setposts(user_posts.data)
+    }
+    console.log(datauser)
+
+    
     setloading(false)
   }
   
-  return (
-    <div className="h-100 text-light mt-5 pt-2">
+  return (<>
         {loading ?
-          <Spinner types="ripple" /> : 
-        <>
-          <div className="card my-2 border border-secondary bg-dark text-light">
-            <div className="card-body">
-              <strong>{userData.email}</strong>
+          <div className="h-100"><Spinner types="ripple" /></div> : 
+          <div className="container mt-5 py-5">
+            <div className="card my-2 mb-3 border border-secondary bg-dark text-light">
+              <div className="card-body">
+                <strong>{userData.email}</strong>
+              </div>
+            </div>
+
+            <div className="container-fuid mt-5 mb-3">
+            <div className="ms-3 mb-3 text-light"><h5>Posts</h5></div>
+              <Posts posts={posts} />
             </div>
           </div>
-        </>
         }
-    </div>
+  </>
   )
 }
 
