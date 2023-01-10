6113eeee3e0c2bee6fd709abdd48d2bb59daaf61
Reza Firdaus <azerus98@gmail.com>
2023-01-10 11:11:59 +0700

    add spinner/loader

--- pages/profile/[user_email].tsx
+++ pages/profile/[user_email].tsx
@@ -1,7 +1,11 @@
 import { GetServerSideProps } from "next"
+import dynamic from "next/dynamic"
 import { useEffect, useState } from "react"
 import { getAPI } from "../../lib/callAPI"
+import { getData } from "../../lib/dataStore"
 import UserTypes from "../../types/users"
+
+const Spinner = dynamic(() => import('../../components/spinner'), { ssr: false })
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const { query } = ctx
@@ -22,19 +26,38 @@ const userpage = (props: { user_email: string }): JSX.Element => {
   }, [])
 
   const fetchData = async () => {
-    const res = await getAPI({path: `users/0?email=${props.user_email}`})
-    const userdata: UserTypes = res.data
-    
-    if (userdata) {
-      setuserData(userdata)
-    } else console.log(res.message)
+    const email = getData('email', 0)
+    if (props.user_email == email) {
+      const user = {
+        email,
+        user_id: getData('user_id', 0)
+      }
+
+      setuserData(user)
+    } else {
+      const res = await getAPI({path: `users/0?email=${props.user_email}`})
+      const userdata: UserTypes = res.data
+      
+      if (userdata) {
+        setuserData(userdata)
+      } else console.log(res.message)
+    }
 
     setloading(false)
   }
   
   return (
-    <div className="text-light mt-5 pt-2">
-        {loading ? <span>Loading...</span> : <span>{userData.email}</span>}
+    <div className="h-100 text-light mt-5 pt-2">
+        {loading ?
+          <Spinner types="ripple" /> : 
+        <>
+          <div className="card my-2 border border-secondary bg-dark text-light">
+            <div className="card-body">
+              <strong>{userData.email}</strong>
+            </div>
+          </div>
+        </>
+        }
     </div>
   )
 }
